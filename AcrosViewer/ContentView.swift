0e5f54eedca08415c8501937d3d7e71ed3e8a117
Guye Raphael, I233 <raphael.guye@post.ch>
2020-07-15 22:19:46 +0200

    Initial Commit

---
+++ AcrosViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AcrosViewer
+//
+//  Created by Guye Raphael, I233 on 15.07.20.
+//  Copyright © 2020 Guye Raphael. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
