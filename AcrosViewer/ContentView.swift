cffbcba1da48609a9c872396367996de6691ed81
Guye Raphael, I233 <raphael.guye@post.ch>
2020-08-05 10:14:09 +0200

    Implement basis of the modal page

--- AcrosViewer/ContentView.swift
+++ AcrosViewer/ContentView.swift
@@ -10,18 +10,39 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var isFilterDisplayed = false
+    
     var body: some View {
         NavigationView {
             AcroListView()
             .navigationBarItems(trailing: filterButton())
+            .sheet(isPresented: $isFilterDisplayed) {
+                self.filterPage()
+            }
         }
     }
     
     private func filterButton() -> some View {
         return Button(action: {
-            NSLog("clicked")
+            self.isFilterDisplayed = true
         }) {
             Image(systemName: "line.horizontal.3.decrease.circle")
+        }
+    }
+    
+    private func filterPage() -> some View {
+        return NavigationView {
+            Text("This is the filter page")
+                .navigationBarTitle("Filter", displayMode: .inline)
+                .navigationBarItems(trailing: doneButton())
+        }
+    }
+    
+    private func doneButton() -> some View {
+        return Button(action: {
+            self.isFilterDisplayed = false
+        }) {
+            Text("Done")
         }
     }
 }
